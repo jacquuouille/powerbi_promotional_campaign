7cfb6a79b9b23b93a9a5e340c125c5fa8ec0ee50
Jacques Hervochon <62380585+jacquuouille@users.noreply.github.com>
2025-12-12 16:49:43 +0100

    Fix comment for redemption rate calculation

--- sql/redemption_rate.sql
+++ sql/redemption_rate.sql
@@ -1,4 +1,4 @@
--- 1. Redemption rate by role (% completion divided by % sent (not divided by % viewed)
+-- 1. Redemption rate (% completion divided by % sent (not divided by % viewed)
 
 with 
 offer_stat as ( 
