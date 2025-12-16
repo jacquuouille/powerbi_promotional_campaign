fc4cf785867dfb0edadb9b0e9a25fdfc45d98555
Jacques Hervochon <62380585+jacquuouille@users.noreply.github.com>
2025-12-16 22:16:31 +0100

    Update materialised_views.sql

--- sql/materialised_views.sql
+++ sql/materialised_views.sql
@@ -77,7 +77,7 @@ AS
   			else event 
 			end as event
   
-   -- 'value' -> to unnest in order to have 'offer_id', 'reward' and 'amount' as extra columns. Extracting the data this way removed duplicates (374 duplicate events (≈2.5%)).
+   -- 'value' -> to unnest in order to have 'offer_id', 'reward' and 'amount' as extra columns. Extracting the data this way removes duplicates (374 duplicate events (≈2.5%)).
 	 	, case 
   			when value like '%offer id%' then regexp_replace(value, '.*offer id'':[[:space:]]*''([^'']+)''.*', '\1') 
   			when value like '%offer_id%' then regexp_replace(value, '.*offer_id'':[[:space:]]*''([^'']+)''.*', '\1')
